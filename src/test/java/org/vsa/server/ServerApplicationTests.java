b2a4fb022de387b408a6f975f59b9945e604a7d1
bbabbi <mooncheayoung@gmail.com>
2024-12-24 15:26:39 +0900

    [FEAT] 테스트 코드 실행 시 csrf 비활성화

---
+++ src/test/java/org/vsa/server/ServerApplicationTests.java
@@ -0,0 +1,26 @@
+package org.vsa.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@SpringBootTest
+class ServerApplicationTests {
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http
+				.csrf(csrf -> csrf.disable())
+				.authorizeHttpRequests(auth -> auth
+						.anyRequest().permitAll()
+				);
+
+		return http.build();
+	}
+
+	@Test
+	void contextLoads() {
+	}
+
+}
